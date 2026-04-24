5a75caf4e4257a39de5bbd884bb93a68cc0be0e5
lytKateryna <lyt.kateryna@gmail.com>
2026-04-24 13:55:34 +0200

    added files

--- library/permissions.py
+++ library/permissions.py
@@ -7,7 +7,23 @@
 Для всех остальных аутентифицированных пользователей возвращает 403.
 Подключить permission к эндпоинтам списка и детализации пользователей.
 Для анонимного пользователя возвращать 401.
-
+Задание 2
+пользователь может смотреть только свой профиль
+Эндпоинты:
+Требования
+Реализовать permission, который:
+Разрешает доступ, если тот, кто делает запрос -- это владелец объекта.
+Администратору разрешает доступ к любому объекту пользователя.
+Разрешить редактирование только своих данных:
+first_name
+last_name
+phone
+Запретить изменение:
+role
+is_staff
+is_superuser
+deleted
+При нарушении прав возвращать 403 с понятным сообщением.
 """
 
 from rest_framework.permissions import BasePermission
@@ -24,4 +40,10 @@ class IsAdminOnly(BasePermission):
 
 
     def has_object_permission(self, request, view, obj):
+        if view.action == 'get_me':
+            if request.user and request.user.is_authenticated and request.user.role == 'admin'
+                return True
+            elif request.user and request.user.is_authenticated and request.user.id == obj.id:
+                return True
+
         return request.user and request.user.is_authenticated and request.user.role == 'admin'
--- library/views/users.py
+++ library/views/users.py
@@ -1,8 +1,11 @@
+from rest_framework.generics import get_object_or_404
 from rest_framework.viewsets import ModelViewSet
 from library.models import User
 from library.serializers import UserSerializer
 from library.permissions import IsAdminOnly
-
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import action
 
 
 
@@ -10,3 +13,10 @@ class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAdminOnly]
+
+    @action(detail=False, methods=['get'], url_path='me')
+    def get_me(self, request, *args, **kwargs):
+        # queryset = User.objects.filter(id=request.user.id).first() - first wariant
+        obj = get_object_or_404(User, pk=request.user.id)
+        serializer = self.get_serializer(obj)
+        return Response(serializer.data, status.HTTP_200_OK)
